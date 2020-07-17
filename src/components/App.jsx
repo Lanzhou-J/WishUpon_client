ef961ed4be64617c1b43eff08c7bb78c0a225201
Lanzhou Jiang <lzjiang99@gmail.com>
2020-07-17 12:40:41 +1000

    get wishes data from rails app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import NoMatch from "./NoMatch";
+import Wishes from "./Wishes";
 // import CreateWish from "./CreateWish";
 
 
@@ -14,7 +15,7 @@ class App extends React.Component {
           {/* <Route exact path="/wishes/create" component={CreateWish} /> */}
           {/* <Route exact path="/wishes/:id/edit" component={EditWish} /> */}
           {/* <Route exact path="/wishes/:id" component={Wish} /> */}
-          {/* <Route exact path="/wishes" component={Wishes} /> */}
+          <Route exact path="/wishes" component={Wishes} />
           {/* <Route exact path="/login" component={Login} /> */}
           {/* <Route exact path="/sign-up" component={SignUp} /> */}
           <Route exact path="/" component={Home} />
---
+++ src/components/Wishes.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+class Wishes extends React.Component {
+  state = { wishes: [] };
+
+  getWishes = async () => {
+    const response = await fetch("http://localhost:3000/wishes/", {
+      // headers: {
+      //   'Authorization': `Bearer ${localStorage.getItem('token')}`
+      // }
+    });
+    const data = await response.json();
+    this.setState({ wishes: data.wishes });
+    console.log(this.state);
+  };
+
+  renderWishes = () => {
+    return this.state.wishes.map((wish, index) => {
+      return (
+        <div className="wish-index" key={index}>
+          {/* <Link to={{ pathname: `/wishes/${wish.id}`, state: wishes }}>           */}
+          <h3 className="wish-index-title">{wish.title}</h3>
+          <p>Description: {wish.description}</p>
+          {/* </Link> */}
+        </div>
+      );
+    });
+  };
+
+  render() {
+    return (
+      <div className="wish-index-container">
+        {this.renderWishes()}
+      </div>
+    );
+  }
+
+  async componentDidMount() {
+    this.getWishes();
+  }
+}
+
+export default Wishes;
