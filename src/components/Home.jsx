d59256722064dda03adc1579b46a337037ac91c4
Lanzhou Jiang <lzjiang99@gmail.com>
2020-07-19 12:40:10 +1000

    add image and text in Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,14 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        <h1>Home</h1>
+        <div className="image-container">
+          <div className="image-wrapper"></div>
+        </div>
+        <div className="text-container">
+          <h1>Make Your Wishes Come True When You Wish Upon...</h1>
+          <p className="text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias nam molestiae tempora quas dolor quaerat cumque nostrum fugit, assumenda magni?</p>
+        </div>
+
       </div>
     )
   }
