1c5e7d053e09a77bab1da746b630a51f0d454907
Lanzhou Jiang <lzjiang99@gmail.com>
2020-07-22 00:13:14 +1000

    add comments part in wish page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,7 @@ import Navbar from "../shared/Navbar";
 import "../stylesheets/App.scss";
 import Room from "../shared/Room";
 
+
 class App extends React.Component {
   render() {
     return (
--- src/components/Wish.jsx
+++ src/components/Wish.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 // import moment from 'moment';
 
 class Wish extends React.Component {
-  state = { wishes: null };
+  state = { wishes: null, comments: null };
   deleteWish = async (id) => {
     await fetch(`http://localhost:3000/wishes/${id}`, {
       method: "DELETE",
@@ -26,16 +26,42 @@ class Wish extends React.Component {
     this.setState({ wishes: data});
   }
 
+  showComment = async(id) => {
+    const response = await fetch(`http://localhost:3000/wishes/${id}/comments`, {
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      }
+    });
+    const data = await response.json();
+    // console.log(data)
+    this.setState({ comments: data});
+  }
+
+  renderComments = () => {
+    return this.state.comments.comments.map((comment, index) => {
+      return (
+        <div className="wish-index" key={index}>  
+          <p> {comment.content}</p>
+          <hr />
+        </div>
+      );
+    });
+  };
+
   componentDidMount(){
     const b = this.props.match.params.id;
-    console.log(b)
+    // console.log(b)
     this.showWish(b)
+    this.showComment(b)
   }
 
   render() {
     console.log(this.state)
     const wish = this.state.wishes
-    if(wish){
+    const comments = this.state.comments
+    // console.log(comments)
+    if(wish&&comments){
+      console.log(comments.comments)
       return (
         <div className="wish-view">
           <h1>Wish</h1>
@@ -47,6 +73,10 @@ class Wish extends React.Component {
           <span onClick={() => this.deleteWish(wish.id)}>
             <button className="edit-back-delete-button" >Delete</button>
           </span>
+          <div className="comments">
+           <h3>Comments:</h3>
+          {this.renderComments()}
+          </div>
         </div>
       );
     }else{
