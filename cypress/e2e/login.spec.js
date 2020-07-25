f95d90649c2985d366323375a8ac9aa94dab4f20
DannyField <dannyfieldproductions@gmail.com>
2020-07-25 22:21:12 +1000

    tests for login working

--- cypress/e2e/login.spec.js
+++ cypress/e2e/login.spec.js
@@ -1,9 +1,24 @@
+// Displays the log in form
 describe("login form", () => {
   it("renders email and password inputs", () => {
     cy.visit("/login");
     // test code
   });
 });
+
+// This test will log the admin user in
+describe("tries to log in", () => {
+  it("logs in with correct creds", () => {
+    cy.visit("/");
+    cy.findByText(/Login/).click();
+    cy.get("#email").type("admin@wishupon.com");
+    cy.get("#password").type("password");
+    cy.get("#login-button").click();
+    cy.url().should("eql", "http://localhost:8080/wishes");
+  });
+});
+
+// This test will try to log a new user in and will fail. It should send them to sign up
 
 // describe("clicking on login", () => {
 //   it("should go to the login page", () => {
