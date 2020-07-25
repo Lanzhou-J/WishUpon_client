f07d6b555514ea9bc4b93421e61aaea4748bc299
DannyField <dannyfieldproductions@gmail.com>
2020-07-25 22:39:01 +1000

    tests working for logging in and out as a registered user

--- cypress/e2e/login.spec.js
+++ cypress/e2e/login.spec.js
@@ -7,18 +7,30 @@ describe("login form", () => {
 });
 
 // This test will log the admin user in
-describe("tries to log in", () => {
-  it("logs in with correct creds", () => {
+describe("tries to log in and log out", () => {
+  it("logs in with correct creds and logs out", () => {
     cy.visit("/");
     cy.findByText(/Login/).click();
     cy.get("#email").type("admin@wishupon.com");
     cy.get("#password").type("password");
     cy.get("#login-button").click();
     cy.url().should("eql", "http://localhost:8080/wishes");
+    cy.findByText(/Logout/).click();
+    cy.url().should("eql", "http://localhost:8080/login");
   });
 });
 
 // This test will try to log a new user in and will fail. It should send them to sign up
+// describe("tries to log in", () => {
+//   it("logs in with incorrect creds and sends user to sign up", () => {
+//     cy.visit("/");
+//     cy.findByText(/Login/).click();
+//     cy.get("#email").type("harrison_and_ed@coder.com");
+//     cy.get("#password").type("weforgot");
+//     cy.get("#login-button").click();
+//     cy.url().should("eql", "http://localhost:8080/signup");
+//   });
+// });
 
 // describe("clicking on login", () => {
 //   it("should go to the login page", () => {
